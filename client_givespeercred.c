c7ec3d4b9d55501a91cbeba600ac0f012ce9961a
Preston Moore <prestonkmoore@gmail.com>
2019-06-02 22:59:18 +0100

    New progs where server requests from client

---
+++ client_givespeercred.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+  int sockfd;
+  if(-1 == (sockfd = socket(AF_UNIX, SOCK_STREAM, 0))) {
+    perror("socket creation failed");
+  }
+  struct sockaddr_un addr;
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, "/tmp/testsocket");
+
+  if(-1 == connect(sockfd, (struct sockaddr*)&addr, sizeof(addr))) {
+    perror("connect failed");
+  }
+
+
+  printf("Connected to server \n");
+  printf("Forking in 10 seconds\n");
+  sleep(10);
+
+  pid_t pid;
+  printf("Forking for the first time\n");
+  if(-1 == (pid = fork())) {
+    perror("First fork failed");
+  }
+  if(0 == pid) {
+    printf("We have forked the first time.  Advance the client...\n");
+    printf("Forking again in 10 seconds\n");
+    sleep(10);
+    if(-1 == (pid = fork())) {
+      perror("Second fork failed");
+    }
+    if(0 == pid) {
+      printf("We have forked the second time.  Advance the client...\n");
+      printf("Exiting in 10 seconds\n");
+      sleep(10);
+      close(sockfd);
+      unlink("/tmp/testsocket");
+    } else {
+      printf("Second fork generated child process pid: %d\n", pid);
+      exit(0);
+    }
+  } else {
+    printf("First fork generated child process pid: %d\n", pid);
+    exit(0);
+  }
+
+}
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all: server_givespeercred client_requestspeercred
+all: server_givespeercred client_requestspeercred server_requestspeercred client_givespeercred
 
 server_givespeercred: server_givespeercred.c
 	gcc -std=c11 -o server_givespeercred server_givespeercred.c
@@ -6,6 +6,14 @@ server_givespeercred: server_givespeercred.c
 client_requestspeercred: client_requestspeercred.c
 	gcc -std=c11 -o client_requestspeercred client_requestspeercred.c
 
+server_requestspeercred: server_requestspeercred.c
+	gcc -std=c11 -o server_requestspeercred server_requestspeercred.c
+
+client_givespeercred: client_givespeercred.c
+	gcc -std=c11 -o client_givespeercred client_givespeercred.c
+
 clean:
 	- rm server_givespeercred
 	- rm client_requestspeercred
+	- rm server_requestspeercred
+	- rm client_givespeercred
